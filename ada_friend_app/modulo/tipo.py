b2ab150e9f9f0f2f535cdc064baceddd2184f60a
Rodrigo Guimarães <rodrigoara27@gmail.com>
2019-06-22 04:01:44 -0300

    persisintindo tipos

--- ada_friend_app/modulo/tipo.py
+++ ada_friend_app/modulo/tipo.py
@@ -3,9 +3,10 @@ from ada_friend_app.modulo.jwt_auth import Token
 
 def set_tipo(token, tipos):
     infos = Token.validar_token(token)
-    usuarios = Database().get_document('usuarios', filter=
+    usuario = Database().get_document('usuarios', filter=
     {'$and':[{'_id':infos['email']}, {'senha':infos['senha']}]})
-    if usuario
-    
+    if usuario:
+        pass
+
 
     return infos
