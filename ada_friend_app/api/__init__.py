72bfbcd4b739838be882eefac96d6ca532c0b5f1
Rodrigo Guimarães <rodrigoara27@gmail.com>
2019-06-22 00:06:38 -0300

    cadastro

--- ada_friend_app/api/__init__.py
+++ ada_friend_app/api/__init__.py
@@ -1,5 +1,7 @@
 from ada_friend_app.api.status import Status
+from ada_friend_app.api.cadastro import Cadastro
 
 
 def add_resources(api):
     api.add_resource(Status, '/api/status')
+    api.add_resource(Cadastro, '/api/cadastrar')
