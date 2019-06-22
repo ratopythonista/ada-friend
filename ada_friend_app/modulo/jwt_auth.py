94cf9dd5fd5a83ef57d9dcac272ccfe375dc046a
Rafael <bsb.rafaelaraujo@gmail.com>
2019-06-22 02:23:43 -0300

    correção token

--- ada_friend_app/modulo/jwt_auth.py
+++ ada_friend_app/modulo/jwt_auth.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 
 import jwt
-from jwt.exceptions import ExpiredSignatureError
 
 from ada_friend_app.config import JWT_PWD, JWT_EXP
 
@@ -10,15 +9,13 @@ class Token:
     # TODO: ALTERAR PARA HORAS A VALIDAÇÃO DO TOKEN
     def __init__(self, senha, id_usuario):
         self.token = self.__token(senha, id_usuario)
+        self.info_token = None
 
     def __token(self, senha, id_usuario):
         return jwt.encode({'pwd': senha, 'id_usuario': id_usuario, 'exp': datetime.utcnow() + timedelta(seconds=JWT_EXP)}, JWT_PWD, algorithm='HS256')
 
-    def validar_token(self):
-        try:
-            self.token = self.__token(jwt.decode(self.token, JWT_PWD, algorithms='HS256'))
-        except ExpiredSignatureError:
-            return None
-        
-        return self.token
+    def validar_token(self, token):      
+        self.info_token = jwt.decode(bytes(token, encoding='utf-8'), JWT_PWD, algorithms='HS256')
+        self.__token(self.info_token)
 
+        return self.info_token
